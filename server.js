0f918f4b57903b51f9d58a04a7e46c1e1fde6e1a
Mahesh Kumar Bisai <119428669+MaheshKumarBisai@users.noreply.github.com>
2025-05-16 18:19:37 +0530

    Update server.js
    Updated an js

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-// vulnerable.js
 
 const http = require('http');
 const url = require('url');
 
 http.createServer((req, res) => {
   const query = url.parse(req.url, true).query;
-  
-  // Vulnerability 1: Command Injection (simulated)
-  const exec = require('child_process').exec;
-  exec(`echo ${query.input}`, (error, stdout, stderr) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(stdout);
-  });
 
-  // Vulnerability 2: Unsafe eval usage
-  if (query.code) {
-    eval(query.code);  // Dangerous!
-  }
+  // Safe way: do NOT use user input in exec or eval
+  // Instead, just send a safe response
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(`Hello ${query.name ? query.name.replace(/</g, "&lt;").replace(/>/g, "&gt;") : 'Guest'}`);
 
-  // Vulnerability 3: Possible XSS (not escaped output)
-  res.write(`<h1>Hello ${query.name}</h1>`);
 }).listen(8080);
